61662afde2d25d99ecf59e7a1d585e3082f5054f
R-Currie <currierobertm@gmail.com>
2019-12-02 22:31:12 -0600

    Can shuffle cards

--- src/App.js
+++ src/App.js
@@ -6,6 +6,30 @@ import {importedFlashCardDataFile} from './flashCardData';
 const cardData = importedFlashCardDataFile;
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            cardData: []
+        };
+    }
+
+    componentDidMount() {
+        this.setState({ cardData });
+    }
+
+    shuffleBackward = () => {
+        const { cardData } = this.state;
+        const newCardData = [cardData[6], ...cardData.slice(0, 6)];
+        this.setState({ cardData: newCardData });
+    }
+
+    shuffleForward = () => {
+        const { cardData } = this.state;
+        const newCardData = [...cardData.slice(1), cardData[0]];
+        this.setState({ cardData: newCardData });
+    }
+
     render() {
         return(
             <div className="app">
@@ -16,7 +40,15 @@ class App extends React.Component {
                     An interactive flashcard app.
                 </p>
                 <div className='card-list'>
-                    {cardData.map((card, index) => <Card front={card.front} key={index} />)}
+                    {this.state.cardData.map((card, index) => <Card front={card.front} key={index} />)}
+                </div>
+                <div className="buttons">
+                    <button onClick={() => this.shuffleBackward()}>
+                        &lt;
+                    </button>
+                    <button onClick={() => this.shuffleForward()}>
+                        &gt;
+                    </button>
                 </div>
             </div>
         );
